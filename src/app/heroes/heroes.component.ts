4b80ffda08b7bc56ae3603e987575a64e69d9677
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2022-04-16 11:27:20 -0300

    Implementação do dataservice. Busca de produtos.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,6 +20,19 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero).subscribe(
+      hero => this.heroes.push(hero)
+    );
+  }
+
+  deleteHero(hero: Hero) {
+    this.heroes = this.heroes.filter(h => h !== h);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
   getHeroes() {
     return this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
